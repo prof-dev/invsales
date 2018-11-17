2021190f16ae34ed46a8c6452e6c11a6f2105954
mohamedmelsayed <mohamedosmanelsayed@yahoo.com>
2018-11-17 14:56:15 +0200

    design

--- src/app/components/lookups/lookups.component.ts
+++ src/app/components/lookups/lookups.component.ts
@@ -3,6 +3,7 @@ import { HttpService } from 'src/app/services/http.service';
 import { ShareService } from 'src/app/services/share.service';
 import { Router } from '@angular/router';
 import { Banks, Tresuries } from 'src/app/classes/Lookup';
+import { ifStmt } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-lookups',
@@ -16,6 +17,8 @@ export class LookupsComponent implements OnInit {
   public lookupitems: any[] = [];
   public group: any=null;
   public visible: boolean;
+  public operation:string="new";
+  public form:boolean=false;
   
   public bank={
     group:"",
@@ -28,9 +31,10 @@ export class LookupsComponent implements OnInit {
   };
 
   public data={
-    address:"",balance:0
+    address:"",balance:0, price: null
   }
   public tresury: Tresuries;
+  filterby: any;
 
   
   constructor(private _hs: HttpService,
@@ -58,27 +62,54 @@ export class LookupsComponent implements OnInit {
 
   }
   setGroup(group) {
+    this.form=false;
     this.group = group;
-    this._hs.get('lookups', 'filter=parent,eq,' + group.id)
+    if(group.group=="new"){
+      this.filterby=0;
+    }
+    else{
+      this.filterby=group.id;
+    }
+    this._hs.get('lookups', 'filter=parent,eq,' + this.filterby)
       .subscribe(res => {
         this.lookupitems = res.json().lookups;
       })
       this.visible=true;
+      console.log("group is set to:",group.group);
+      
  
   }
 
   modify(item) {
+    this.form=true;
+    item.data=JSON.parse(item.data);
+    this.bank.titlear=item.titlear;
+    this.data=item.data;
+    this.bank.id=item.id;
+    this.bank.group=item.group;
+    this.bank.isleaf=item.isleaf;
+    this.bank.parent=item.parent;
+    this.operation="تعديل";
+    this.visible = false;
 
   }
   delete(item) {
-
+    this._hs.delete('lookups',item.id) .subscribe(res => {
+      console.log("deletion result:"+res);
+      if(res.status==200){
+        this.ngOnInit();
+      }
+      
+    });
   }
 
   addnew() {
+    this.form=true;
+    this.operation="إدخال جديد";
     this.bank.titlear="fs";
     this.data.address="";
+    this.data.balance=0;
     this.visible = false;
-    document.getElementById('addNew').style.visibility="visible";
     if (this.group.group == 'banks') {
       this.bank.group='bank';
       this.bank.parent=this.group.id;
@@ -89,7 +120,12 @@ export class LookupsComponent implements OnInit {
   }
 
   save(item){
+    
     if(item.id==null){
+     if(this.group.group!='new'){
+      item.parent=this.group.id;
+     }
+      
       item.data=JSON.stringify(this.data);
       console.log(item.data);
       
@@ -98,8 +134,34 @@ export class LookupsComponent implements OnInit {
         confirm("تمت إضافة "+this.group.titlear+" id :"+res);
 
       })
+    
           
      
     }
+    else{
+
+      if(this.group.group!='new'){
+        item.parent=this.group.id;
+       }else {
+         item.parent=this.group.id;
+       }
+
+      item.data=JSON.stringify(this.data);
+      console.log(item.id);
+      
+      this._hs.put('lookups',item.id,item).subscribe(res => {
+
+        console.log("تمت تعديل "+this.group.titlear+" id :"+res);
+
+      })
+    }
+    if(this.group.group!='new'){
+      // this._router.navigateByUrl('/lookups'); 
+      this.ngOnInit();
+      this.form=false;
+      
+     }
   }
+
+
 }
