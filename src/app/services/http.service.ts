1c3c51239782f6d21dccdfcade9724df8bb11fbe
mohamedmelsayed <mohamedosmanelsayed@yahoo.com>
2018-11-25 13:25:33 +0200

    report image

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -15,7 +15,7 @@ export class HttpService {
     if (environment.production) {
       this.APISERVER = 'http://www.prof-dev.com/';
     }else{
-      this.APISERVER = 'http://localhost/invsales/';
+      this.APISERVER = 'http://localhost:83/';
     }
     this.HOST=this.APISERVER+'api.php/';
   }
