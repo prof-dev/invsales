1bb4d7efe12a69032720c7e990e43ae47e4f0e39
mohamedmelsayed <mohamedosmanelsayed@yahoo.com>
2018-12-01 21:39:37 +0200

    class & subitems (inventory)

--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/services/http.service';
 import { ShareService } from 'src/app/services/share.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-inventory',
@@ -12,23 +13,35 @@ export class InventoryComponent implements OnInit {
   public user: any;
   public display = "";
   public entrytype = "";
-  public touched:number[]=[];
-  public actionstore=0;
+  public touched: number[] = [];
+  public actionstore = 0;
   public storeselect: any[] = [];
   public lookups: any[] = [];
   public productsview: any[] = [];
   public items: Item[] = [];
-  public item: any = {};
+  public item = {
+    id: 0,
+    avb: 0,
+    rsv: 0,
+    com: 0
+  };
   public inventroy = {
     storeid: 0,
     data: null
   }
   public stores: any[] = [];
+  public classes: any[] = [];
   public inventories: any[] = [];
   public store: any = {};
+  public goodsobj: any = {};
+  public selectedclass:any={};
   result: string;
   istouched: boolean;
-   isexist: boolean;
+  isexist: boolean;
+  public processinfo={
+    parent:0
+  }
+  public list:any[]=[];
   constructor(private _hs: HttpService,
     private _ss: ShareService,
     private _router: Router) { }
@@ -39,27 +52,12 @@ export class InventoryComponent implements OnInit {
       if (this.user.id == 0) {
         this._router.navigateByUrl('/login');
       }
-      this.item = {
-        id: 0,
-        avb: 0,
-        rsv: 0,
-        com: 0
-      };
+     
       ////   retrieve from inventroy to list items in each stock
-      this._hs.get('inventory')
-        .subscribe(res => {
-          this.inventories = res.json().inventory;
-          console.log(this.inventories);
-
-          this.inventories.forEach(element => {
-
-            this.items = JSON.parse(element.data);
-            element.data = this.items;
-            console.log(this.items);
-          });
-        });
+      this.getinventories();
 
       this.getallitems();
+      this.getallclasses();
 
 
 
@@ -84,6 +82,19 @@ export class InventoryComponent implements OnInit {
     });
   }
 
+  private getinventories() {
+    this._hs.get('inventory')
+      .subscribe(res => {
+        this.inventories = res.json().inventory;
+        console.log(this.inventories);
+        this.inventories.forEach(element => {
+          this.items = JSON.parse(element.data);
+          element.data = this.items;
+          console.log(this.items);
+        });
+      });
+  }
+
   private getallitems() {
     this._hs.get('lookups', 'filter=group,eq,item')
       .subscribe(res => {
@@ -93,42 +104,74 @@ export class InventoryComponent implements OnInit {
           element.data = JSON.parse(element.data);
           console.log(this.items);
         });
-        this.productsview = this.lookups.map(lookup => ({ id: lookup.id, titlear: lookup.titlear, price: lookup.data.price }));
+        this.productsview = this.lookups.map(lookup => ({ id: lookup.id, titlear: lookup.titlear, price: lookup.data.price ,parent:lookup.parent}));
+      this.list=this.productsview;
       });
   }
 
-  additemtolist(item) {
-    console.log(this.items.length);
-    this.isexist=false;
-    this.items.forEach(element => {
-     
-      if (element.id == item.id) {
-        this.isexist=true;
-        element.avb =element.avb+parseInt(item.avb);
-        element.rsv =  element.rsv+parseInt(item.rsv);
-        element.com =  element.com+parseInt(item.com);
-      }
+  private getallclasses() {
+    this._hs.get('lookups', 'filter=group,eq,goods')
+      .subscribe(res => {
+        this.goodsobj = res.json().lookups[0];
+        // console.log(this.goodsobj);
+        this._hs.get('lookups', 'filter[]=parent,eq,' + this.goodsobj.id)
+          .subscribe(res => {
+            this.classes = res.json().lookups;
+            console.log(this.classes);
+            
+          });
+      });
+  }
+
+  getitemsbyclass(){
+    if(this.processinfo.parent!=0){
+      console.log(this.processinfo.parent);
+      console.log(this.productsview);
       
+      console.log(this.list);
+      
+      this.list=this.productsview.filter(option => option.parent==this.processinfo.parent);
+      console.log(this.list);
+    }
+   
+    
+  }
+
+
+  additemtolist(item) {
+    console.log(item.id);
+    
+    console.log(this.items.length);
+    this.isexist = false;
+    this.items.forEach(element => {
+
+      if (element.id == item.id) {
+        this.isexist = true;
+        element.avb = element.avb + parseInt(item.avb);
+        element.rsv = element.rsv + parseInt(item.rsv);
+        element.com = element.com + parseInt(item.com);
+      }
+
     });
-    if(!this.isexist){
-      item.avb=parseInt(item.avb);
-      item.id=parseInt(item.id);
-      item.rsv=parseInt(item.rsv);
-      item.com=parseInt(item.com);
+    if (!this.isexist) {
+      item.avb = parseInt(item.avb);
+      item.id = parseInt(item.id);
+      item.rsv = parseInt(item.rsv);
+      item.com = parseInt(item.com);
       this.items.push(item);
     }
     this.inventories.forEach(element => {
-      if(element.storeid==this.actionstore){
-        element.data=this.items;
+      if (element.storeid == this.actionstore) {
+        element.data = this.items;
       }
     });
-    this.istouched=false;
+    this.istouched = false;
     this.touched.forEach(element => {
-      if(element==this.actionstore){
-        this.istouched=true;
+      if (element == this.actionstore) {
+        this.istouched = true;
       }
     });
-    if(!this.istouched){
+    if (!this.istouched) {
       this.touched.push(this.actionstore);
     }
 
@@ -143,18 +186,18 @@ export class InventoryComponent implements OnInit {
 
   }
 
-  openinsertitem(list: any[], storeid,type) {
+  openinsertitem(list: any[], storeid, type) {
     this.items = [];
     this.items = list;
     this.entrytype = type;
-    if(storeid!=""){
+    if (storeid != "") {
       this.display = this.getstorename(storeid);
-      this.actionstore=storeid;
+      this.actionstore = storeid;
     }
-    
-    
+
+
     console.log(this.display);
-    
+
   }
 
   getstorename(id): string {
@@ -187,22 +230,22 @@ export class InventoryComponent implements OnInit {
     return this.result;
   }
 
-  save(){
+  save() {
     this.touched.forEach(outer => {
       this.inventories.forEach(inner => {
-        if(outer==inner.storeid){
-          inner.data=JSON.stringify(inner.data);
-          this._hs.put('inventory','storeid',inner).subscribe(res =>{
-     
+        if (outer == inner.storeid) {
+          inner.data = JSON.stringify(inner.data);
+          this._hs.put('inventory', 'storeid', inner).subscribe(res => {
+
             this._ss.setSnackBar('inserted page is refreshed for you');
           });
         }
       });
     });
-    
-    this.touched=[];
+
+    this.touched = [];
     this.ngOnInit();
-    
+
   }
 
 }
