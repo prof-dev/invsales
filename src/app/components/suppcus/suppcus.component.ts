00dba52585abdb8eb7185082df937870ae7f1925
Abubaker Shangab <shangab@gmail.com>
2018-12-01 13:54:04 +0300

    done

--- src/app/components/suppcus/suppcus.component.ts
+++ src/app/components/suppcus/suppcus.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { ShareService } from '../../services/share.service';
 import { HttpService } from '../../services/http.service';
 import { Router } from '@angular/router';
-
+declare var ol: any;
 interface Data {
     phone: string;
     phone2: string;
@@ -24,7 +24,7 @@ interface Suppcus {
     styleUrls: ['./suppcus.component.css']
 })
 export class SuppcusComponent implements OnInit {
-
+    map: any;
     public suppcus: Suppcus[] = [];
     public theader: any = [];
     public data: Data;
@@ -33,76 +33,86 @@ export class SuppcusComponent implements OnInit {
 
     public form: any;
 
-    public operation=0;
-    message: string;
+    public operation = 0;
+    location: any;
 
     constructor(private _ss: ShareService, private _hs: HttpService, private _router: Router) { }
     ngOnInit() {
-        this.operation=0;
+        
+        this.map = new ol.Map({
+            target: 'map',
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                }),
+
+            ],
+            view: new ol.View({
+                center: ol.proj.fromLonLat([32.546012, 15.51056]),
+                zoom: 14
+            })
+        });
+        this.map.on('singleclick', (ev)=>{
+            this.location = ol.proj.toLonLat(ev.coordinate);
+            console.log('location: ', ev.coordinate)
+        });
+        this.operation = 0;
         this._ss.User.subscribe(user => {
             this.user = user;
             if (this.user.id == 0) {
-              this._router.navigateByUrl('/login');
+                this._router.navigateByUrl('/login');
             } else {
-                this.form = {
-                    "type": "",
-                    "id": 0,
-                    "balance": 0.00,
-                    "fullname": "",
-                    "branch":"",
-                    "data": {
-                        "phone": "",
-                        "whatsapp": "",
-                        "email": "",
-                        "phone2": ""
-                        // "location" "4555,55555"
-                    }
-                }
-      
-              
+                this.resetForm();
             }
-          });
-    
+        });
+
 
     }
 
-
+    resetForm() {
+        this.form = {
+            "type": "",
+            "id": 0,
+            "balance": 0.00,
+            "fullname": "",
+            "branch": "",
+            "data": {
+                "phone": "",
+                "whatsapp": "",
+                "email": "",
+                "phone2": ""
+                // "location" "4555,55555"
+            }
+        }
+    }
     save(form) {
-        if(form.id!=0){
+        if (form.id != 0) {
             form.data = JSON.stringify(form.data);
             console.log(form.data);
-            this._hs.put('suppcus', "id",form).subscribe(res => {
-             
-                   this._ss.setSnackBar('تم حفظ  '+ this.formtype+' بنجاح');
-                   
-          
-              
-
+            this._hs.put('suppcus', "id", form).subscribe(res => {
+                this._ss.setSnackBar('تم حفظ  ' + this.formtype + ' بنجاح');
             });
-            this.ngOnInit();
-           
-        }
-        else 
-        if (form.fullname != "") {
-            form.data = JSON.stringify(form.data);
-            console.log(form.data);
-            this._hs.post('suppcus', form).subscribe(res => {
+            this.resetForm();
 
-                this.message ='تم حفظ  '+ this.formtype+' بنجاح';
-
-            });
-            this.ngOnInit();
         }
-        else {
-            this.message = 'الرجاء ملء جميع الحقول';
-        
-        }
+        else
+            if (form.fullname != "") {
+                form.data = JSON.stringify(form.data);
+                console.log(form.data);
+                this._hs.post('suppcus', form).subscribe(res => {
+                    this._ss.setSnackBar('تم حفظ  ' + this.formtype + ' بنجاح');
+                });
+                this.resetForm();
+            }
+            else {
+                this._ss.setSnackBar('الرجاء ملء جميع الحقول');
+            }
     }
 
 
     setType(type) {
         this.ngOnInit();
-        this.operation=1;
+        this.operation = 1;
         this.form.type = type;
 
         if (this.form.type == 's') {
@@ -112,8 +122,8 @@ export class SuppcusComponent implements OnInit {
             this._hs.get('suppcus', 'filter=type,eq,s')
                 .subscribe(res => {
                     this.suppcus = res.json().suppcus;
-                    this.suppcus .forEach(element => {
-                        element.data=JSON.parse(element.data);
+                    this.suppcus.forEach(element => {
+                        element.data = JSON.parse(element.data);
                     });
                 })
 
@@ -121,36 +131,33 @@ export class SuppcusComponent implements OnInit {
         else if (this.form.type == 'c') {
             this.formtype = 'العملاء';
             this._hs.get('suppcus', 'filter=type,eq,c')
-            .subscribe(res => {
-                this.suppcus = res.json().suppcus;
-                this.suppcus .forEach(element => {
-                    element.data=JSON.parse(element.data);
-                    console.log(element.data);
-                    
-
-                });
-
-            })
+                .subscribe(res => {
+                    this.suppcus = res.json().suppcus;
+                    this.suppcus.forEach(element => {
+                        element.data = JSON.parse(element.data);
+                        console.log(element.data);
+                    });
+                })
         }
     }
     onNoClick() {
-        this.ngOnInit();
+        this.resetForm();
     }
 
 
-    modify(item){
-        this.operation=2;
-        if(item.type=='s'){
-            this.formtype='تعديل الموردين';
+    modify(item) {
+        this.operation = 2;
+        if (item.type == 's') {
+            this.formtype = 'تعديل الموردين';
 
         }
-        else if (item.type=='c'){
-            this.formtype='تعديل العملاء';
+        else if (item.type == 'c') {
+            this.formtype = 'تعديل العملاء';
 
         }
 
-        this.form=item;
-     //   this.form.data=JSON.parse(item.data);
+        this.form = item;
+        //   this.form.data=JSON.parse(item.data);
 
     }
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -16,7 +16,7 @@ export class HttpService {
     if (environment.production) {
       this.APISERVER = 'http://www.prof-dev.com/';
     }else{
-      this.APISERVER = 'http://localhost:83/';
+      this.APISERVER = 'http://www.prof-dev.com/';
     }
     this.HOST=this.APISERVER+'api.php/';
   }
